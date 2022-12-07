f1677ef1fe2e956f8224bd75ac1cb9f0e51a9d25
Giv3My <bessvlad45@gmail.com>
2022-12-07 22:25:58 +0200

    About window added

---
+++ Windows/About.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MovieApp.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для About.xaml
+    /// </summary>
+    public partial class About : Window
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
