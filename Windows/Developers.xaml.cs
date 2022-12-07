491f2bfed64efca09892dc822c9cec5563e0d37e
Giv3My <bessvlad45@gmail.com>
2022-12-07 22:26:18 +0200

    Developers window added

---
+++ Windows/Developers.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MovieApp.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Developers.xaml
+    /// </summary>
+    public partial class Developers : Window
+    {
+        public Developers()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
