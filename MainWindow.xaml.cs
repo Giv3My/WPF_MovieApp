50dcc22d07eeb5b0da0a885e3b6ed5f0c14f8b9d
Giv3My <bessvlad45@gmail.com>
2022-12-07 22:16:15 +0200

    Main window added

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using MovieApp.MVVM.View;
+using MovieApp.Windows;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MovieApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AppContent.Content = new MovieListing();
+        }
+
+        private void CloseApp(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MinimizeApp(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (movieListingBtn.IsChecked == true)
+            {
+                AppContent.Content = new MovieListing();
+            }
+            else if (randomMovieBtn.IsChecked == true)
+            {
+                AppContent.Content = new RandomMovie();
+            }
+            else
+            {
+                AppContent.Content = new Favorites();
+            }
+        }
+
+        private void ShowAboutWindow(object sender, MouseButtonEventArgs e)
+        {
+            About AboutWindow = new About();
+
+            AboutWindow.Show();
+        }
+
+        private void ShowDevelopersWindow(object sender, MouseButtonEventArgs e)
+        {
+            Developers DevelopersWindow = new Developers();
+
+            DevelopersWindow.Show();
+        }
+    }
+}
